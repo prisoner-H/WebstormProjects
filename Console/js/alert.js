c5524a2d93ff4150f0089ff5c74f052c8e713cdc
HU <hfkvip0126@163.com>
2021-04-18 15:24:55 +0800

    Initial commit

---
+++ Console/js/alert.js
@@ -0,0 +1,20 @@
+function taoLu(){
+    alert("你猜一会会发生什么");
+    alert("再等等看");
+    alert("说不定真有奇迹呢");
+}
+
+alert("你怎么打开了这个网页");
+
+taoLu();
+
+alert("你相信奇迹吗");
+alert("问个问题吧");
+
+taoLu();
+alert("奇迹的几率太低了");
+alert("不过也不一定");
+
+taoLu();
+
+alert("这是最后一句话了");
